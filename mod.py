c09ff62399e44a726a52ed3c0a0c14d704bdf09a
smkisvarday <susannahkisvarday@hotmail.com>
2021-09-20 18:41:54 -0400

    fixed mistakes

---
+++ mod.py
@@ -0,0 +1,17 @@
+s = "If Comrade Napoleon says it, it must be right."
+a =  [100, 200, 300]
+print ('a=', a)
+
+def foo (arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass
+
+if(__name__=='__main__'):
+    print('Executing as standalone script')
+    print(s)
+    print(a)
+    foo('quux')
+    x = Foo()
+    print(x)
